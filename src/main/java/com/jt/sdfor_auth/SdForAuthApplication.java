926e13de84e9ccd1b4ddc5dff48b1591a88ede77
kurt92 <chcch1234@gmail.com>
2024-11-05 17:50:21 +0900

    init

---
+++ src/main/java/com/jt/sdfor_auth/SdForAuthApplication.java
@@ -0,0 +1,13 @@
+package com.jt.sdfor_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdForAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdForAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdfor_auth/SdForAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdfor_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdForAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
