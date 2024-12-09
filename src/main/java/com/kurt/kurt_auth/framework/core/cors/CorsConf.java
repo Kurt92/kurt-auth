ad519a78031dbf03918d164c1151af23d5c7cf7e
kurt92 <chcch1234@gmail.com>
2024-12-09 23:31:01 +0900

    feat : ec2 https 적용후 secure ture로 변경

--- src/main/java/com/kurt/kurt_auth/framework/core/cors/CorsConf.java
+++ src/main/java/com/kurt/kurt_auth/framework/core/cors/CorsConf.java
@@ -10,11 +10,11 @@ public class CorsConf implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 클라이언트 도메인
+                .allowedOrigins("https://localhost:3000") // 클라이언트 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true) // 쿠키를 포함한 요청 허용
-//                .allowedHeaders("*") // 모든 헤더 허용
-//                .exposedHeaders("Set-Cookie"); // 쿠키 반환 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .exposedHeaders("Set-Cookie"); // 쿠키 반환 허용
         ;
     }
 }
