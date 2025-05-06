f6531470d95e927eb3b8f2fdf5de025a34d3e34a
kurt92 <chcch1234@gmail.com>
2025-05-07 02:35:32 +0900

    fix : prod 환경테스트

--- src/main/java/com/kurt/auth/framework/core/Redis/RedisConf.java
+++ src/main/java/com/kurt/auth/framework/core/Redis/RedisConf.java
@@ -1,46 +1,45 @@
-//package com.kurt.auth.framework.core.Redis;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//
-//@Configuration
-//public class RedisConf {
-//
-//
-//    @Value("${spring.data.redis.host}")
-//    private String host;
-//
-//    @Value("${spring.data.redis.port}")
-//    private int port;
-//
+package com.kurt.auth.framework.core.Redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConf {
+
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+
+//    여기서 자꾸 로컬호스트로 연결하길래 ㅈㄴ 삽질함 ㅆㅂ
+//    야믈파일 설정을 참조 안하길래 보니까 여기서 빈 생성을 하면 기본값으로 생성함
+//    할꺼면 위에처럼 명시적으로 넣어줘야하고
 //    @Bean
 //    public RedisConnectionFactory redisConnectionFactory() {
-//        return new LettuceConnectionFactory(host, port);
+//        return new LettuceConnectionFactory();
 //    }
-//
-//
-////    여기서 자꾸 로컬호스트로 연결하길래 ㅈㄴ 삽질함 ㅆㅂ
-////    야믈파일 설정을 참조 안하길래 보니까 여기서 빈 생성을 하면 기본값으로 생성함
-////    할꺼면 위에처럼 명시적으로 넣어줘야하고
-////    이 설정 자체가 필요 없다함.
-////    @Bean
-////    public RedisConnectionFactory redisConnectionFactory() {
-////        return new LettuceConnectionFactory();
-////    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(redisConnectionFactory());
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.setValueSerializer(new StringRedisSerializer());
-//        return template;
-//    }
-//
-//}
-//
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+}
+
